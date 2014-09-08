3031d7b8cb00130f745481fcc677126dc69416f3
maksaraswat <maksaraswat@gmail.com>
2014-09-07 20:41:30 -0400

    Create arp_ping.py

---
+++ arp_ping.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import string, sys
+from scapy.all import *
+
+def arpping(host):
+	try:
+		ans,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=host),timeout=2)
+#		ans.summary(lambda (s,r): r.sprintf("%Ether.src% %ARP.psrc%") )		
+		for s,r in ans:
+			print "{0} is the MAC address for host {1}".format(r.src, r.psrc)
+	except Exception, e:
+		print e
+
+def main():
+	arpping(host)
+
+if __name__ == '__main__':
+	main()
