cbc3c65ec38d4aaf8531a17de882f539665c16d4
maksaraswat <maksaraswat@gmail.com>
2014-09-07 20:40:33 -0400

    Create icmp_ping.py

---
+++ icmp_ping.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import string, sys
+from scapy.all import *
+
+def icmpping(host):
+	try:
+		ans,unans=sr(IP(dst=host)/ICMP(), inter=0.05,timeout=0.5)
+#		ans.summary(lambda (s,r): r.sprintf(" [*] %IP.src% is alive and is Linux" if r.ttl < 65 else " [*] %IP.src% is alive and is Windows"))
+		for s,r in ans:
+			
+			if r.ttl < 65:
+				print "{0} is alive and is Linux(TTL = {1})".format(str(r.src), str(r.ttl))
+			else:
+				print "{0} is alive and is Windows(TTL = {1})".format(str(r.src), str(r.ttl))	
+	except Exception, e:
+		print e
+
+def main():
+	icmpping(host)
+
+if __name__ == '__main__':
+	main()
